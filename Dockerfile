6cd2330ad7f523e53975bd4ccfe5b154edbd4691
red171 <red171@applejuicenet.cc>
2019-12-06 15:19:13 +0100

    * add more env vars 4 docker

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,6 @@ FROM php:7-apache
 ARG BUILD_DATE
 ARG VCS_REF
 
-ENV CORE_HOST ""
-ENV CORE_PORT ""
-
 RUN apt update && apt install -y --no-install-recommends libpng-dev \
     && docker-php-ext-install gd \
     && apt clean
@@ -21,3 +18,19 @@ LABEL org.label-schema.name="appleJuice phpGUI" \
       org.label-schema.build-date=${BUILD_DATE} \
       org.label-schema.vcs-url="https://bitbucket.org/red171/applejuice-phpgui" \
       org.label-schema.schema-version="1.0"
+
+ENV CORE_HOST ""
+ENV CORE_PORT 9851
+
+ENV GUI_LANGUAGE "deutsch"
+ENV GUI_STYLE "tango"
+
+ENV GUI_REFRESH_STATUS 10
+ENV GUI_REFRESH_DOWNLOADS 30
+ENV GUI_REFRESH_UPLOADS 30
+ENV GUI_REFRESH_SEARCH 30
+
+ENV GUI_OUTPUT_COMPRESS 1
+
+ENV GUI_SHOW_NEWS 1
+ENV GUI_SHOW_SHARE 1
--- vars.php
+++ vars.php
@@ -1,24 +1,20 @@
 <?php
 //Language
-$standard_language_xml = 'deutsch.xml';
-//$standard_language_xml='english.xml';
+$standard_language_xml = (!empty($_ENV['GUI_LANGUAGE']) ? $_ENV['GUI_LANGUAGE'] : 'deutsch') . '.xml';
 
 //Standard Login Data
 $core_standard_ip = !empty($_ENV['CORE_HOST']) ? $_ENV['CORE_HOST'] : $_SERVER['REMOTE_ADDR'];
 $core_standard_xml_port = !empty($_ENV['CORE_PORT']) ? $_ENV['CORE_PORT'] : 9851;
+$core_standard_host = $core_standard_ip . ':' . $core_standard_xml_port;
 
 //Style
-$standard_stylefile = 'new.php';
-//$standard_stylefile='tango.php';
-//$standard_stylefile='default_green.php';
-//$standard_stylefile='default_blue.php';
-//$standard_stylefile='default_grey.php';
+$standard_stylefile = (!empty($_ENV['GUI_STYLE']) ? $_ENV['GUI_STYLE'] : 'new') . '.php';
 
 //reload intervall (in seconds)
-$_SESSION['reloadtime']['status'] = 40;
-$_SESSION['reloadtime']['downloads'] = 80;
-$_SESSION['reloadtime']['uploads'] = 80;
-$_SESSION['reloadtime']['search'] = 80;
+$_SESSION['reloadtime']['status'] = !empty($_ENV['GUI_REFRESH_STATUS']) ? $_ENV['GUI_REFRESH_STATUS'] : 10;
+$_SESSION['reloadtime']['downloads'] = !empty($_ENV['GUI_REFRESH_DOWNLOADS']) ? $_ENV['GUI_REFRESH_DOWNLOADS'] : 30;
+$_SESSION['reloadtime']['uploads'] = !empty($_ENV['GUI_REFRESH_UPLOADS']) ? $_ENV['GUI_REFRESH_UPLOADS'] : 30;
+$_SESSION['reloadtime']['search'] = !empty($_ENV['GUI_REFRESH_SEARCH']) ? $_ENV['GUI_REFRESH_SEARCH'] : 30;
 
 //allowed tags in servermessage
 $_SESSION['phpaj']['allowed_servermsg_tags'] = '<a><b><i><u><br>';
@@ -36,8 +32,8 @@ $_SESSION['phpaj']['autocleandownloadlist'] = 0;
 $_SESSION['phpaj']['progressbars_type'] = 1;
 
 //use gzip compression
-$_SESSION['phpaj']['zipped'] = 1;
+$_SESSION['phpaj']['zipped'] = !empty($_ENV['GUI_OUTPUT_COMPRESS']) ? $_ENV['GUI_OUTPUT_COMPRESS'] : 1;
 
 //Start
-$start_shownews = 1;
-$start_showshareinfo = 1;
+$start_shownews = !empty($_ENV['GUI_SHOW_NEWS']) ? $_ENV['GUI_SHOW_NEWS'] : 1;
+$start_showshareinfo = !empty($_ENV['GUI_SHOW_SHARE']) ? $_ENV['GUI_SHOW_SHARE'] : 1;
