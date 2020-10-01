83666f3d8c3066b7351b60f6de292f17bde5567f
red171 <red171@appledocs.to>
2020-10-01 10:42:07 +0200

    allow tab selection in perma link

--- main/index.php
+++ main/index.php
@@ -11,6 +11,13 @@ if (empty($_SESSION['core_host'])) {
     header('Location: ../index.php');
     die;
 }
+
+$tabs = ['start', 'downloads', 'uploads', 'shares', 'search', 'server', 'settings', 'extras'];
+
+$tab = 'start';
+if (isset($_GET['tab']) && in_array($_GET['tab'], $tabs, true)) {
+    $tab = $_GET['tab'];
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -23,7 +30,7 @@ if (empty($_SESSION['core_host'])) {
 </head>
 <frameset rows="70,*,30" border="0">
     <frame src="top.php" name="oben" frameborder="0" scrolling="no" noresize="noresize"/>
-    <frame src="start.php" name="main" frameborder="0" noresize="noresize"/>
+    <frame src="<?php echo $tab; ?>.php" name="main" frameborder="0" noresize="noresize"/>
     <frame src="status.php" name="status" frameborder="0" scrolling="no" noresize="noresize"/>
 </frameset>
 </html>
