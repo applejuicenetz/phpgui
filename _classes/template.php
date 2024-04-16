14328302d3fe3dcd31afaef6a9ac2fd4e936d229
Kddk22 <161597664+Kddk22@users.noreply.github.com>
2024-04-16 10:50:36 +0200

    Update alerts

--- _classes/template.php
+++ _classes/template.php
@@ -16,12 +16,19 @@ class template{
 	}
 	function alert($alert, $strong, $text){
 		// $alert = success, warning, danger, info
+		if($alert == "success") $icon = "check-circle";
+		if($alert == "info") $icon = "info-circle";
+		if($alert == "warning") $icon = "exclamation-triangle";
+		if($alert == "danger") $icon = "exclamation-triangle";
+		
 		echo' <div class="alert alert-'.$alert.'" role="alert">
                                     <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                    <i class="fa fa-fw fa-check-circle"></i>
+                                    <i class="fa fa-fw fa-'.$icon.'"></i>
                                     <strong>'.$strong.'</strong> '.$text.'
                                 </div>
                                ';
 	}
-
+	function errors(){
+	
+	}
 }
