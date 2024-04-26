d7f40f9a8d97174fa9c9651ebc5160aec23c2f87
Kddk22 <161597664+Kddk22@users.noreply.github.com>
2024-04-26 11:26:06 +0200

    new icons

--- src/GUI/Icons.php
+++ src/GUI/Icons.php
@@ -5,7 +5,6 @@ namespace appleJuiceNETZ\GUI;
 class Icons
 {
     var $os;
-    var $serverstatus;
     var $directstate;
     var $os_system;
 
@@ -13,10 +12,10 @@ class Icons
     {
         //Passendes symbol (oder name) fuer betriebssystem der id zuordnen
         $this->os = array(
-            "0" => '<img width="24" height="24" src="https://img.icons8.com/color/48/operating-system--v1.png" alt="operating-system--v1"/>',
-            "1" => '<img width="24" height="24" src="https://img.icons8.com/color/24/windows-10.png" alt="windows-10"/>',
-            "2" => '<img width="24" height="24" src="https://img.icons8.com/color/24/linux--v1.png" alt="linux--v1"/>',
-            "3" => '<img width="24" height="24" src="https://img.icons8.com/color/24/mac-logo.png" alt="mac-logo"/>',
+            "0" => '<img width="16" height="16" src="https://img.icons8.com/color/48/operating-system--v1.png" alt="operating-system--v1"/>',
+            "1" => '<img width="16" height="16" src="themes/icons/windows_os.svg" alt="windows-10"/>',
+            "2" => '<img width="16" height="16" src="themes/icons/linux_os.svg" alt="linux--v1"/>',
+            "3" => '<img width="16" height="16" src="themes/icons/mac_os.svg" alt="mac-logo"/>',
             "4" => "<img src='../style/"
                 . $_SESSION['os_solaris_icon'] . "' alt='solaris' />",
             "5" => "<img src='../style/"
@@ -39,22 +38,11 @@ class Icons
             "7" => "<img src='../style/"
                 . $_SESSION['os_netware_icon'] . "' alt='netware' />");
 
-        //ALT Zum Serverstatus passendes symbol anzeigen
-        $this->serverstatus = array(
-            "alt" => "<img src='../style/"
-                . $_SESSION['server_old_icon'] . "' alt='old' />",
-            "neu" => "<img src='../style/"
-                . $_SESSION['server_new_icon'] . "' alt='new' />",
-            "verbinde" => "<img src='../style/"
-                . $_SESSION['server_connecting_icon'] . "' alt='connecting' />",
-            "verbunden" => "<img src='../style/"
-                . $_SESSION['server_connected_icon'] . "' alt='connected' />");
-
         //direkte/indirekte verbindung
         $this->directstate = array(
-            "0" => "<img src='/themes/icons/unknow.png' alt='?' />",
-            "1" => "<img src='/themes/icons/direct.png' alt='direct' />",
-            "2" => "<img src='/themes/icons/indirect.png' alt='indirect' />",
-            "3" => "<img src='/themes/icons/indirect.png' alt='indirect' />");
+            "0" => "<img src='themes/icons/unknow.svg' width='16' alt='?' />",
+            "1" => "<img src='themes/icons/direct.svg' width='16' alt='direct' />",
+            "2" => "<img src='themes/icons/indirect.svg' width='16' alt='indirect' />",
+            "3" => "<img src='themes/icons/indirect.svg' width='16' alt='indirect' />");
     }
 }
